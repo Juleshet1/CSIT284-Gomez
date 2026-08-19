0ad9a8ae67b6e49ea819ba3c20be53477ce447ce
LAB12_USER ON WS31 <L12X19W31@LabHCI.net>
2026-08-19 20:51:57 +0800

    add parameter to StyledText

--- lib/styled_text.dart
+++ lib/styled_text.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 
-const textStyle = TextStyle(
-  fontSize: 24,
-  color: Color.fromARGB(255, 102, 4, 4),
-);
+const StyledText extends StatelessWidget{
+const StyledText (this.text, {super.key}));
+final String text;
+@override
+Widget build(BuildContext context) {
+  return Text(
+    text,
+    style: textStyle.copyWith(fontSize: 48, color: Color.fromARGB(255, 41, 2, 2)),
+  );
+}
